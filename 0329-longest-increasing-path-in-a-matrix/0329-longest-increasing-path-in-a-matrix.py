7735317258e0a9ddea4f650f1eefa4934fbebfbc
ravicmi <65382744+ravicmi@users.noreply.github.com>
2025-02-07 01:33:54 +0530

    Time: 190 ms (37.46%), Space: 22.7 MB (26.46%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -5,15 +5,18 @@ class Solution:
         if not matrix: return 0
         m, n = len(matrix), len(matrix[0])
         
-        # Memoization for DP
-        @lru_cache(None)
+        dict_dfs = {(i,j): -1 for i in range(m) for j in range(n)}
+
         def dfs(i, j):
+            if dict_dfs[(i,j)]  != -1 :
+                return dict_dfs[(i,j)]
             directions = [(0,1), (1,0), (0,-1), (-1,0)]  # Right, Down, Left, Up
             max_path = 1
             for dx, dy in directions:
                 ni, nj = i + dx, j + dy
                 if 0 <= ni < m and 0 <= nj < n and matrix[ni][nj] > matrix[i][j]:
                     max_path = max(max_path, 1 + dfs(ni, nj))
+            dict_dfs[(i,j)] = max_path
             return max_path
 
         return max(dfs(i, j) for i in range(m) for j in range(n))
