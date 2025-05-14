9faf854602931a8fbfad471fb8ce618b0d74aef3
ravicmi <65382744+ravicmi@users.noreply.github.com>
2025-05-14 13:09:47 +0530

    Time: 707 ms (30.77%), Space: 20.6 MB (84.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,59 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        from collections import defaultdict
-        d= defaultdict(int)
-        for i in nums:
-            d[i] +=1
-        s = sorted(d.keys())
-        l = 0
-        length = len(s)
-        result = []
-        for l in range(length):
-            for r in range(length-1, l, -1):
-                mid = -(s[l] + s[r])
-                if (s[l]<mid) and (s[r]>mid) and d[mid]>0:
-                    result.append([mid, s[l], s[r]])
-                if (s[l]==mid) and (s[r]>mid) and d[mid]>1:
-                    result.append([mid, s[l], s[r]])
-                if (s[l]<mid) and (s[r]==mid) and d[mid]>1:
-                    result.append([mid, s[l], s[r]])
-                if (s[l]==mid) and (s[r]==mid) and d[mid]>2:
-                    result.append([mid, s[l], s[r]])
-        if d[0]>2:
-            result.append([0,0,0])
+        out = []
+        nums.sort()
+        n = len(nums)
+        i = 0 
+        while i<= n-2:
+            if nums[i]>0:
+                break
+            j = i+1
+            k = n-1
+            while j<k:
+                sum3 = nums[i]+nums[j]+nums[k]
+                if sum3==0:
+                    out.append([nums[i], nums[j], nums[k]])
+                    while k>j and nums[k]==nums[k-1] :
+                        k -=1
+                    while  k>j and nums[j]==nums[j+1] :
+                        j+=1
+                    k-=1
+                    j+=1
+                elif sum3 > 0:
+                    while k>j and nums[k]==nums[k-1] :
+                        k -=1
+                    k -=1
+                else: 
+                    while k>j and nums[j]==nums[j+1]:
+                        j+=1
+                    j+=1
+            while i<=n-2 and nums[i]==nums[i+1]:
+                i+=1
+            i+=1
+        return out
 
-        return result 
 
 
 
+
+
+
+
+
+        # from collections import defaultdict
+        # dict = defaultdict(list)
+        # for index, i in enumerate(nums): 
+        #     dict[i].append(index)
+        # n = len(nums)
+        # out = set()
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         ni = nums[i]
+        #         nj = nums[j]
+        #         nk = -ni-nj
+        #         for k in dict[nk]:
+        #             if k>j:
+        #                 a,b,c = sorted([ni, nj,nk])
+        #                 out.add((a,b,c))
+        # out = list(out)
+        # return out
