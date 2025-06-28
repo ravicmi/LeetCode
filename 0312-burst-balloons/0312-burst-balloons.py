3fa488786b85349db46782711d604274358157e2
ravicmi <65382744+ravicmi@users.noreply.github.com>
2025-06-28 15:20:18 +0530

    Time: 4555 ms (56.61%), Space: 34.4 MB (6.87%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,34 +1,17 @@
-# class Solution:
-#     def maxCoins(self, nums: List[int]) -> int:
-#         from functools import lru_cache
-#         nums = [1] + nums +[1]
-#         n = len(nums)
-#         @lru_cache
-#         def dp(i, j):
-#             if j <= i+1:
-#                 return 0
-#             a = 0 
-#             for mid in range(i+1, j):
-#                 loc = nums[i]*nums[mid]*nums[j] + dp(i, mid) + dp(mid, j)
-#                 a = max(loc, a)
-#             return a
-#         return dp(0, n-1)
-
-
-
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         from functools import lru_cache
-        nums = [1] + nums + [1]
+        nums = [1] + nums +[1]
         n = len(nums)
-
         @lru_cache(None)
-        def dp(left, right):
-            if right - left <= 1:
+        def dp(i, j):
+            if j <= i+1:
                 return 0
-            return max(
-                nums[left] * nums[i] * nums[right] + dp(left, i) + dp(i, right)
-                for i in range(left + 1, right)
-            )
+            a = 0 
+            for mid in range(i+1, j):
+                loc = nums[i]*nums[mid]*nums[j] + dp(i, mid) + dp(mid, j)
+                a = max(loc, a)
+            return a
+        return dp(0, n-1)
 
-        return dp(0, n - 1)
+
