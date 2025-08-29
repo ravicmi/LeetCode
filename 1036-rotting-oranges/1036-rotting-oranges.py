03d9f0f67e7848a915f68fe7aee323b752ff9817
ravicmi <65382744+ravicmi@users.noreply.github.com>
2025-08-30 00:25:41 +0530

    Time: 5 ms (34.78%), Space: 17.8 MB (71.57%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -2,7 +2,7 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         dir = [(-1,0), (1,0), (0,1), (0,-1)]
         rot = []
-        good = set()
+        good = 0
         level = 0
         m = len(grid)
         n = len(grid[0])
@@ -11,7 +11,7 @@ class Solution:
                 if grid[i][j] == 2: 
                     rot.append((i,j))
                 if grid[i][j] == 1 : 
-                    good.add((i,j))
+                    good +=1
         while rot: 
             next_rot = []
             for rl, rr in rot: 
@@ -19,7 +19,7 @@ class Solution:
                     l,r = rl+nl, rr+nr
                     if l>=0 and l< m and r >=0 and r < n and grid[l][r] == 1:
                         grid[l][r] = 2
-                        good.remove((l,r))
+                        good -= 1
                         next_rot.append((l,r))
             if next_rot:
                 level +=1 
